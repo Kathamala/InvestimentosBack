1a9b6b9bfeae76b811a14a3abba4711154849b0b
Thiago Vinícius Azevedo de Oliveira <thiago.vinicius@smartsuite.imd.ufrn.br>
2023-12-06 20:48:24 -0300

    Initial commit

---
+++ src/main/java/com/example/InvestimentosBack/InvestimentosBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvestimentosBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentosBackApplication.class, args);
+	}
+
+}
